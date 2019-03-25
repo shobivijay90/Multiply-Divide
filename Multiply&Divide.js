419abae7e0cf878689d6ec2556e3c8b36425de00
shobivijay90 <43181177+shobivijay90@users.noreply.github.com>
2019-03-24 18:29:48 -0700

    Add files via upload

---
+++ Multiply&Divide.js
@@ -0,0 +1,15 @@
+function multiply(){
+  var a = document.getElementById("value1").value;
+  var b =
+document.getElementById("value2").value;
+  var result = a * b;
+  document.getElementById("answer").innerHTML = result;
+}
+
+function divide(){
+  var a = document.getElementById("value1").value;
+  var b =
+document.getElementById("value2").value;
+  var result = a / b;
+  document.getElementById("answer").innerHTML = result;
+}
